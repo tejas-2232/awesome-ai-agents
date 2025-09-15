34b8ac657d4c4bdf6e1fe6c8b3a0bbc2c54d665f
Tejas Bachhav <tejasbachhav98@gmail.com>
2025-09-15 17:29:58 -0400

    add Automate_event_planning.py

---
+++ crewAI/Automate_event_planning.py
@@ -0,0 +1,9 @@
+# L5: Automate Event Planning
+
+#In this lesson, you will learn more about Tasks.
+
+## Required libraries
+#### !pip install crewai==0.28.8 crewai_tools==0.1.6 langchain_community==0.0.29
+
+import warnings
+warnings.filterwarnings('ignore')
